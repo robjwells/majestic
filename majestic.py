ab06fd30d745529b85ccadeec68b957acedcb6ce
Rob Wells <rob@robjwells.com>
2015-08-21 21:20:32 +0100

    majestic: implement load_settings

---
+++ majestic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import configparser
+import pathlib
+
+MAJESTIC_DIR = pathlib.Path(__file__).resolve().parent
+
+def load_settings(config_files=None):
+    """Load specified config files or the default and local ones"""
+    if config_files is None:
+        default_cfg = MAJESTIC_DIR.joinpath('majestic.cfg')
+        local_cfg = pathlib.Path.cwd().joinpath('settings.cfg')
+        config_files = map(str, [default_cfg, local_cfg])
+    config = configparser.ConfigParser()
+    config.read(config_files)
+    return config
