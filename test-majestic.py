ba94ed03a51e81070e8e5adeafe6aa1d80377d5b
Rob Wells <rob@robjwells.com>
2015-08-21 21:30:14 +0100

    test: load_settings - test with specified file
    With a test settings.cfg file to match

--- test-majestic.py
+++ test-majestic.py
@@ -1,7 +1,9 @@
+import pathlib
 import unittest
 
 import majestic
 
+TEST_BLOG_DIR = pathlib.Path(__file__).resolve().parent.joinpath('test-blog')
 
 class TestLoadSettings(unittest.TestCase):
     """Default and site-specific settings tests"""
@@ -11,6 +13,11 @@ class TestLoadSettings(unittest.TestCase):
         settings = majestic.load_settings()
         self.assertTrue(settings.getboolean('testing', 'default cfg loaded'))
 
+    def test_load_specific_only(self):
+        """When given filenames, load only those files"""
+        test_settings_fn = str(TEST_BLOG_DIR.joinpath('settings.cfg'))
+        settings = majestic.load_settings([test_settings_fn])
+        self.assertTrue(settings.getboolean('testing', 'test-blog cfg loaded'))
 
 
 if __name__ == '__main__':
