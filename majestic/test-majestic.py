79510889a3bdd3c36a43bbc8421311201629fba1
Rob Wells <rob@robjwells.com>
2017-04-08 17:01:08 +0100

    alter tests for json config, add json config

--- majestic/test-majestic.py
+++ majestic/test-majestic.py
@@ -39,16 +39,16 @@ class TestLoadSettings(unittest.TestCase):
         os.chdir(str(MAJESTIC_DIR))
 
     def test_load_default_settings(self):
-        """Config class contains setting set only in default .cfg file"""
+        """Config class contains setting set only in default config file"""
         settings = majestic.load_settings(default=True, local=False)
-        self.assertTrue(settings.getboolean('testing', 'default cfg loaded'))
+        self.assertTrue(settings['testing']['default cfg loaded'])
 
     def test_load_specific_only_str(self):
         """When given filenames (as str), load only those files"""
-        test_settings_fn = str(TEST_BLOG_DIR.joinpath('settings.cfg'))
+        test_settings_fn = str(TEST_BLOG_DIR.joinpath('settings.json'))
         settings = majestic.load_settings(default=False, local=False,
                                           files=[test_settings_fn])
-        self.assertTrue(settings.getboolean('testing', 'test-blog cfg loaded'))
+        self.assertTrue(settings['testing']['test-blog cfg loaded'])
 
     def test_load_specific_only_Path(self):
         """When given filenames (as pathlib.Path), load only those files
@@ -58,28 +58,26 @@ class TestLoadSettings(unittest.TestCase):
         The burden of conversion should fall on the function
         itself, not its callers.
         """
-        test_settings_fn = TEST_BLOG_DIR.joinpath('settings.cfg')
+        test_settings_fn = TEST_BLOG_DIR.joinpath('settings.json')
         settings = majestic.load_settings(default=False, local=False,
                                           files=[test_settings_fn])
-        self.assertTrue(settings.getboolean('testing', 'test-blog cfg loaded'))
+        self.assertTrue(settings['testing']['test-blog cfg loaded'])
 
     def test_load_default_and_local(self):
-        """Properly load defaults and settings.cfg in current directory"""
+        """Properly load defaults and settings.json in current directory"""
         os.chdir(str(TEST_BLOG_DIR))
         settings = majestic.load_settings(default=True, local=True)
-        self.assertTrue(settings.getboolean('testing', 'test-blog cfg loaded'))
-        self.assertTrue(settings.getboolean('testing', 'default cfg loaded'))
+        self.assertTrue(settings['testing']['test-blog cfg loaded'])
+        self.assertTrue(settings['testing']['default cfg loaded'])
 
     def test_defaults_overriden_by_local(self):
         """Config files loaded in order so that locals override defaults"""
         default_settings = majestic.load_settings(default=True, local=False)
-        overridden_value = default_settings.getboolean('testing',
-                                                       'overridden setting')
+        overridden_value = default_settings['testing']['overridden setting']
         self.assertFalse(overridden_value)
         os.chdir(str(TEST_BLOG_DIR))
         combined_settings = majestic.load_settings()
-        overridden_value = combined_settings.getboolean('testing',
-                                                        'overridden setting')
+        overridden_value = combined_settings['testing']['overridden setting']
         self.assertTrue(overridden_value)
 
     def test_settings_empty_when_not_given_anything(self):
