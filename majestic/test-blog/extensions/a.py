cfefad17668f7f668c954dd60b9be5239ae28303
Rob Wells <rob@robjwells.com>
2017-04-08 23:47:02 +0100

    Add dummy extension to test-blog directory

--- majestic/test-blog/extensions/a.py
+++ majestic/test-blog/extensions/a.py
@@ -1,3 +1,6 @@
+import markdown.extensions
+
+
 def process_posts_and_pages(*, posts, pages, settings):
     """Dummy processer that sets an attribute on posts and pages"""
     for post in posts:
@@ -6,8 +9,15 @@ def process_posts_and_pages(*, posts, pages, settings):
         page.test_attr = 'page'
     return {'posts': posts, 'pages': pages}
 
+
 def process_objects_to_write(*, objects, settings):
     """Dummy processer that sets an attribute on all objects"""
     for obj in objects:
         obj.test_attr = 'obj'
     return {'objects': objects}
+
+
+class DummyMarkdownExtension(markdown.extensions.Extension):
+    """Dummy Markdown extension so we hit the code path in tests"""
+    def extendMarkdown(self, md, md_globals):
+        pass
